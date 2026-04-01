71abe6bdbca4153a7b7aa4c8e693dcf00b34da2b
MAYANK SAINI <Mayank Saini>
2026-04-01 09:45:34 +0530

    Basic OOPs

--- BASICS/oops.java
+++ BASICS/oops.java
@@ -1,6 +1,76 @@
 
-public class oops {
-     public static void main(String[] args) {
 
-     }
-}
+
+
+// public class oops{
+//       public static void main(String[] args) {
+          
+//           System.out.println("Number of arguments: "+ args.length);
+
+//           for(int i = 0 ; i < args.length ; i++){
+               
+//                System.out.println("Argument " + i + " = " + args[i]);
+
+//           }
+
+//       }
+// }
+
+
+// class A{
+//     int age;
+//    String student;
+//    int rollno;
+//    static String college;
+//    static int grade = 2; // this is also valid for the static block
+
+//    A(String student , int age ,int rollno){
+//        this.student = student;
+//        this.rollno = rollno;
+//        this.age = age;
+//    }
+
+
+// //    block is used to initialize the static values
+
+// //    static {
+// //       college = "IIT Guhawahati";
+// //    }
+      
+// }
+
+
+// class B{
+     
+//     static  final double Pi;
+
+// //     when static is not used ine B class
+//      // B(){
+//      //      Pi = 3.14;
+//      // }
+
+
+// // when static is used in B class
+
+//    static{
+//       Pi = 4.13;
+//    }
+
+// }
+
+// public class oops {
+//      public static void main(String[] args) {
+
+//           A obj = new A("Aditya" , 228 , 101);
+//           A obj2 = new A("Mayank" , 23 , 102);
+
+//           // A.college = "IIT Guhawahati";  first way to write like this
+
+
+//           System.out.println(obj.student + " "+ obj.college + " "+obj.age + " "+obj.rollno);
+//           System.out.println(obj2.student + " "+ obj2.college + " "+obj2.age + " "+obj2.rollno);
+
+
+    
+//      }
+// }
